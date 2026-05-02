d0944b1ff206d1856a602221033c4a61a59cd447
duydo <25020083@vnu.edu.vn>
2026-05-02 22:50:23 +0700

    Merge remote-tracking branch 'origin/checknew' into checknew

--- src/main/java/com/auction/client/Launcher.java
+++ src/main/java/com/auction/client/Launcher.java
@@ -4,6 +4,5 @@ public class Launcher {
     public static void main(String[] args) {
         // Gọi hàm main của MainApp từ đây để lách luật kiểm tra của Java
         MainApp.main(args);
-        //duudyudydudyud
     }
 }
